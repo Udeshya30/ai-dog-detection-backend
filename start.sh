af5d48b92d087e6c7a5630d6f254333deefb3fd8
Udeshya Kumar <udeshya.kumar30@gmail.com>
2025-03-23 00:37:36 +0530

    updated google drive download link for model

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
+
 #!/bin/bash
 
-echo "Downloading YOLOv8 model from Google Drive..."
 mkdir -p models
-wget --no-check-certificate "https://drive.google.com/uc?export=download&id=1OjtmQ3BOtk1RzEYIjiPale1uf7l0BZ9p" -O models/best.pt
 
-echo "Starting FastAPI server..."
+if [ ! -f models/best.pt ]; then
+  echo "Downloading model from Google Drive..."
+  gdown --id 1OjtmQ3BOtk1RzEYIjiPale1uf7l0BZ9p -O models/best.pt
+fi
+
 uvicorn main:app --host=0.0.0.0 --port=10000
